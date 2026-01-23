50f3ee7d886611f3a58d995290d6c3cd5fd3edd4
Mai Văn Thành <138979529+30Sativa@users.noreply.github.com>
2026-01-23 23:37:52 +0700

    Migrate project from TypeScript to JavaScript
    Removed TypeScript configuration files and dependencies, renamed source files from .tsx/.ts to .jsx/.js, and updated ESLint and Vite configs accordingly. The README and .gitignore were updated to reflect the migration. Tailwind config was removed and related plugin kept in Vite config.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -15,13 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='text-3xl font-bold underline text-white'>Vite + React</h1>
+      <h1 className='text-red-500'>Vite + React</h1>
       <div className="card">
-        <button className='bg-red-500 border-4' onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
