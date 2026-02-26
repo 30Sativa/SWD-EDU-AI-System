82fd8835efdde0d5bcf9b30c9aec9fabef67e336
Mai Văn Thành <138979529+30Sativa@users.noreply.github.com>
2026-02-26 22:54:23 +0700

    Add validator for CreateTemplateCourseCommand
    Introduce CreateTemplateCourseValidator (EduAISystem.Application/.../Validators/CreateTemplateCourseValidator.cs) using FluentValidation to validate CreateTemplateCourseCommand. Adds a rule ensuring Request.SubjectId is not empty and provides a Vietnamese error message.

---
+++ EduAISystem/EduAISystem.Application/Features/Courses/Validators/CreateTemplateCourseValidator.cs
@@ -0,0 +1,18 @@
+﻿using EduAISystem.Application.Features.Courses.Commands;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduAISystem.Application.Features.Courses.Validators
+{
+    public class CreateTemplateCourseValidator : AbstractValidator<CreateTemplateCourseCommand>
+    {
+        public CreateTemplateCourseValidator() 
+        {
+            RuleFor(x => x.Request.SubjectId).NotEmpty().WithMessage("SubjectId không được để trống.");
+        }
+    }
+}
