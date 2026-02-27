1f19e280e44f0174ccd206d9fa3e1a635622f0bf
inngok <ngocnguyen120305@gmail.com>
2026-02-28 00:25:38 +0700

    [FEAT] add api for terms management

--- src/features/term/api/termApi.js
+++ src/features/term/api/termApi.js
@@ -7,3 +7,19 @@ export const getTerms = (params) => {
 export const createTerm = (data) => {
     return axiosClient.post('/api/manager/terms', data);
 };
+
+export const getTermById = (id) => {
+    return axiosClient.get(`/api/manager/terms/${id}`);
+};
+
+export const updateTerm = (id, data) => {
+    return axiosClient.put(`/api/manager/terms/${id}`, data);
+};
+
+export const deleteTerm = (id) => {
+    return axiosClient.delete(`/api/manager/terms/${id}`);
+};
+
+export const changeTermStatus = (id, data) => {
+    return axiosClient.patch(`/api/manager/terms/${id}/status`, data);
+};
