c1db338a896c7cee50e72b77d00b066636727731
nguyenvy1904 <nguyenvy19042005@gmail.com>
2026-03-16 12:32:17 +0700

    [FIX] Fix config backend port

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://13.214.167.176',
+        target: 'http://18.143.190.2',
         changeOrigin: true,
         secure: false,
       },
